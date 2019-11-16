e9dbf62becf0b6f2560b411318bed158677671e4
tosin <tosinblessing.shittu@gmail.com>
2019-11-16 15:59:00 +0100

    add frontend view

---
+++ resources/views/frontend/layout/bottom.blade.php
@@ -0,0 +1,17 @@
+<!-- jquery Core-->
+<script src="{{ asset('frontend/js/jquery-2.1.4.min.js')}}"></script>
+
+<!-- Bootstrap -->
+<script src="{{ asset('frontend/js/bootstrap.min.js')}}"></script>
+ 
+
+<!-- Theme Menu -->
+<script src="{{ asset('frontend/js/mobile-menu.js')}}"></script>
+ 
+
+<!-- Owl carousel -->
+<script src="{{ asset('frontend/js/owl.carousel.min.js')}}"></script>
+ 
+
+<!-- Theme Script -->
+<script src="{{ asset('frontend/js/script.js')}}"></script>
